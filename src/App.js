950199bec0669f683fac76e53d6e1dced28642ff
Shivang-cx <wd63.department@gmail.com>
2023-10-12 19:35:40 +0530

    12-10-2023-code-push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import './custom-style.css';
-import Header from './header'
-import Banner from './banner'
+import Header from './components/header';
+import Banner from './components/banner';
+import Discoverdrops from './components/discoverdrops';
 function App() {
   return (
     <div className="App page-main bg-black relative">
       <Header></Header>
       <Banner></Banner>
+     <Discoverdrops></Discoverdrops>
     </div>
   );
 }
